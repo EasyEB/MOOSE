edbce601a7469072856120a62fd2b9b11eebf0b3
Sven Van de Velde <sven.van.de.velde@telenet.be>
2016-12-05 14:26:16 +0100

    update

--- Moose Test Missions/Moose_Test_UNIT_TaskRoute/Moose_Test_UNIT_TaskRoute.lua
+++ Moose Test Missions/Moose_Test_UNIT_TaskRoute/Moose_Test_UNIT_TaskRoute.lua
@@ -3,15 +3,17 @@
 -- This UNIT is one unit from a larger GROUP ...
 local UnitMove = UNIT:FindByName( "UnitMove" )
 
-local UnitPoint = UnitMove:GetPointVec2():RoutePointGround( 5, "cone" )
+local UnitPoint = UnitMove:GetPointVec2():RoutePointGround( 5 )
 
 -- Find the ZONE to move the unit to:
 
 local ZoneMove = ZONE:New( "ZoneMove" )
 
-local ZonePoint = POINT_VEC3:NewFromVec3( ZoneMove:GetVec3(0) ):RoutePointGround(5, "cone")
+local ZonePoint = POINT_VEC3:NewFromVec3( ZoneMove:GetVec3(0) ):RoutePointGround( 5 )
 
-local Task = UnitMove:TaskRoute( {UnitPoint, ZonePoint} )
+local Task = UnitMove:TaskRoute( { UnitPoint, ZonePoint } )
+
+UnitMove:E(Task)
 
 UnitMove:SetTask( Task, 1 )
 
